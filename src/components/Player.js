c85a8382498235753cafbecc067c640fc1c52fd9
Mateus Souza <mateusdesouzapaula97@gmail.com>
2021-01-13 10:37:55 -0300

    add: connectiong buttons, skip-back and skip-foward

--- src/components/Player.js
+++ src/components/Player.js
@@ -14,6 +14,8 @@ const Player = ({
   setSongInfo,
   songInfo,
   audioRef,
+  songs,
+  setCurrentSong,
 }) => {
   //* Event Handlers
   const playSongHandler = () => {
@@ -34,6 +36,20 @@ const Player = ({
     audioRef.current.currentTime = e.target.value;
     setSongInfo({ ...songInfo, currentTime: e.target.value });
   };
+  const skipTrackHandler = (direction) => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+
+    if (direction === "skip-forward") {
+      setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    }
+    if (direction === "skip-back") {
+      if ((currentIndex - 1) % songs.length === -1) {
+        setCurrentSong(songs[songs.length - 1]);
+        return;
+      }
+      setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+    }
+  };
 
   return (
     <div className="player">
@@ -49,14 +65,22 @@ const Player = ({
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
-        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        {/* É colocado uma arrow function ao invés da function direto dentro do onClick pois não queremos que ela execute diretamente */}
+        <FontAwesomeIcon
+          onClick={() => skipTrackHandler("skip-back")}
+          className="skip-back"
+          size="2x"
+          icon={faAngleLeft}
+        />
         <FontAwesomeIcon
           onClick={playSongHandler}
           className="play"
           size="2x"
           icon={isPlaying ? faPause : faPlay}
         />
+        {/* É colocado uma arrow function ao invés da function direto dentro do onClick pois não queremos que ela execute diretamente */}
         <FontAwesomeIcon
+          onClick={() => skipTrackHandler("skip-forward")}
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
